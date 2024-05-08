3982d2b588b5fdee8e9c41fc068d3b2347965dcf
Agustin <molinaenzoagustin@gmail.com>
2024-05-08 12:38:45 -0300

    Organice and arrange operation class and it test

--- new_project_v2/operation.py
+++ new_project_v2/operation.py
@@ -1,14 +1,5 @@
 class Operation:
-  def __init__(self):
-    self._name = ''
-    self._index = -1
-    self._char = ''
-    self._vector_clock = [0,0,0]
-    self._replica_id = ''
-    self._applied = False
-    self._deleted = False
-
-  def __init__(self, name, index, char, vector_clock, replica_id):
+  def __init__(self, name='', index=-1, char='', vector_clock=[0,0,0], replica_id=''):
     self._name = name
     self._index = index
     self._char = char
@@ -23,41 +14,41 @@ class Operation:
   def get_name(self):
     return self._name
 
-  def get_index(self):
-    return self._index
-
-  def get_char(self):
-    return self._char
-
-  def get_clock(self):
-    return self._vector_clock
-
-  def get_replica_id(self):
-    return self._replica_id
-
-  def get_applied(self):
-    return self._applied
-
-  def get_deleted(self):
-    return self._deleted
-
   def set_name(self, name):
     self._name = name
+
+  def get_index(self):
+    return self._index
 
   def set_index(self, index):
     self._index = index
 
+  def get_char(self):
+    return self._char
+
   def set_char(self, char):
     self._char = char
+
+  def get_clock(self):
+    return self._vector_clock
 
   def set_clock(self, vector_clock):
     self._vector_clock = vector_clock
 
+  def get_replica_id(self):
+    return self._replica_id
+
   def set_replica_id(self, replica_id):
     self._replica_id = replica_id
 
+  def get_applied(self):
+    return self._applied
+
   def set_applied(self, applied):
     self._applied = applied
+
+  def get_deleted(self):
+    return self._deleted
 
   def set_deleted(self, deleted):
     self._deleted = deleted
